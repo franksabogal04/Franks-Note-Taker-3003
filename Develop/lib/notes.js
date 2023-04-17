cca4dde9677b15f61fe3eed088c7790ab3b4a232
franksabogal04 <franksabogal04@gmail.com>
2023-04-17 13:40:13 -0400

    finished product

---
+++ Develop/lib/notes.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+
+function createNotesFile(body, notesData) {
+  const note = body;
+  notesData.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, '../db/db.json'),
+    JSON.stringify({ notesData }, null, 2)
+  );
+  return note;
+}
+
+  // re-index 
+
+  let index = 0;
+  notesData.forEach(note => {
+    note.id = index;
+    index += 1;
+  });
+
+  // this deletes the notes that were created
+
+function deleteNotesFile(id, notes) {
+  let notesData = notes.filter(el => {
+    if (el.id == id) {
+      return false
+    } else {
+      return true
+    }
+  })
+
+  //write to file
+
+  fs.writeFileSync(
+    path.join(__dirname, '../db/db.json'),
+    JSON.stringify({ notesData }, null, 2)
+  );
+  return notesData;
+}
+
+module.exports = {
+  createNotesFile,
+  deleteNotesFile
+};
